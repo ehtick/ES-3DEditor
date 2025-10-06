2f95d971783bca0aff40912f26c1ffd80e07dc6d
ErSan <mlt131220@163.com>
2025-10-06 19:55:42 +0800

    fix(Docs): update .gitignore

--- packages/docs/src/.vuepress/.temp/pages/blog/index.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "D:/A-Ersan/astraljs/Astral3D/packages/docs/src/.vuepress/.temp/pages/blog/index.html.vue"
-const data = JSON.parse("{\"path\":\"/blog/\",\"title\":\"博客\",\"lang\":\"zh-CN\",\"frontmatter\":{\"lang\":\"zh-CN\",\"title\":\"博客\",\"draft\":true},\"readingTime\":{\"minutes\":0,\"words\":0},\"git\":{},\"filePathRelative\":null,\"headers\":[],\"type\":\"blog\",\"bulletin\":true}")
-export { comp, data }
