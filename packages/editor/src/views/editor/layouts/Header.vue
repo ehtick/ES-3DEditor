d1caa368682bc2b4b1c266299a62e87142233629
ErSan <mlt131220@163.com>
2025-11-04 23:10:47 +0800

    feat(Editor): 编辑页面点击Logo回到首页

--- packages/editor/src/views/editor/layouts/Header.vue
+++ packages/editor/src/views/editor/layouts/Header.vue
@@ -1,15 +1,23 @@
 <script lang="ts" setup>
 import {inject} from "vue";
+import { useRouter } from 'vue-router';
 import NavigationOperation from '@/components/header/NavigationOperation.vue';
 import RightOperation from '@/components/header/RightOperation.vue';
 import Logo from "@/components/header/Logo.vue";
 
+const router = useRouter();
+
 const sceneInfo = inject<ISceneInfo>("sceneInfo");
+
+function handleLogoClick(){
+  // 回首页
+  router.push('/');
+}
 </script>
 
 <template>
   <div class="flex items-center">
-    <Logo class="w-36px h-36px mr-15px" />
+    <Logo class="w-36px h-36px mr-15px cursor-pointer" @click="handleLogoClick" />
 
     <NavigationOperation />
   </div>
